54db27b0d76d51d5f50e7d42d5d0fdc0e12b3df3
nextlvlpro <80281253+nextlvlpro@users.noreply.github.com>
2023-10-26 14:19:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ mongoose.connect(process.env.DATABASEURL).then(() => { console.log("mongoose is 
 app.use(express.json());
 app.use(cors({
     credentials:true,
-    origin: 'http://localhost:5173/',
+    origin: 'http://localhost:5173',
 }))
 
 app.use(cookieParser())
